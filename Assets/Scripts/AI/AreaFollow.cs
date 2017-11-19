9764d0a93d6a5a4a94b5a444f085b12d0d015a40
André F. Martins <martins.adw@gmail.com>
2017-11-19 15:01:04 -0200

    Adicionar inimigos

---
+++ Assets/Scripts/AI/AreaFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaFollow : MonoBehaviour
+{
+	public float speed;
+	public PlayerTrigger targetTrigger;
+
+	public float gravity = 20.0f;
+
+	private GameObject target;
+	// private Rigidbody rb;
+	private CharacterController controller;
+
+	void Start()
+	{
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		if (players.Length > 1)
+		{
+			Debug.LogError("Multiple players found");
+		}
+
+		target = players[0];
+
+		// rb = GetComponent<Rigidbody>();
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		if (targetTrigger.playerInsideTrigger)
+		{
+			Vector3 direction = Vector3.Normalize(target.transform.position - transform.position);
+			direction *= speed;
+			direction.y = -gravity;
+			// transform.position += Vector3.Normalize(direction) * speed * Time.deltaTime;
+			// rb.velocity = Vector3.Normalize(direction) * speed;
+			controller.Move(direction * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+	[HideInInspector] public bool playerInsideTrigger;
+
+	private BoxCollider trigger;
+
+	void Start()
+	{
+		playerInsideTrigger = false;
+
+		trigger = GetComponent<BoxCollider>();
+		if (trigger == null || !trigger.isTrigger)
+		{
+			Debug.LogError("Missing Trigger");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+	    if (other.CompareTag("Player")) {
+			playerInsideTrigger = true;
+	    }
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+	    if (other.CompareTag("Player")) {
+			playerInsideTrigger = false;
+	    }
+	}
+}
