7edd03ba21db886482afed85d82870ca0bacb029
lucascribeiro <lucascarvalho@ice.ufjf.br>
2017-12-06 18:36:12 -0200

    Som personagem, zumbis e moedas para cena 1

---
+++ Assets/Scripts/Character/WalkAudio.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class WalkAudio : MonoBehaviour {
+    public AudioSource walk;
+    public AudioSource jump;
+    public AudioSource land;
+    private CharacterController controller;
+    bool grounded = true;
+    bool previouslyGrounded = true;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>(); ;
+    }
+
+// Update is called once per frame
+void Update () {
+        if (controller.isGrounded)
+        {
+            grounded = true;
+            if (previouslyGrounded == false)
+                land.Play();
+            previouslyGrounded = true;
+        }
+        else {
+            if (grounded == true)
+                grounded = false;
+            else
+                previouslyGrounded = false;
+        }
+        if (previouslyGrounded && (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right"))) {
+            if (!walk.isPlaying)
+                walk.Play();
+        } else {
+            walk.Stop();
+        }
+
+        if (Input.GetKeyDown("z")) {
+            jump.Play();
+        }
+
+
+        	
+	}
+
+}
+
